d4db94f05f24822bb7c14f4d7a2189bb3e5d57fd
Johnny Zhou <jonathanqyz@gmail.com>
2022-08-27 22:29:48 -0400

    updated module structure

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gameBoard = (() => {
+
   const board = ["", "", "", "", "", "", "", "", ""];
 
   const updateBlock = (index, sign) => {
@@ -19,13 +20,10 @@ const gameBoard = (() => {
   return { updateBlock, getBlock, reset };
 })();
 
-const game = (() => {
-  const winGame = () => {
-    
-  }
-})();
 
-const Player = (sign) => {
+const Player = () => {
+  let sign = 'X'
+
   const getSign = () => {
     return sign;
   }
@@ -33,18 +31,66 @@ const Player = (sign) => {
   return { getSign }
 }
 
+const Game = (() => {
+  const field = document.querySelector('.game-board')
+  const startScreen = document.querySelector('.start-screen')
+  const startBtn = document.querySelector('.start-button')
 
-const playerX = document.querySelector('.player-x')
-const playerO = document.querySelector('.player-o')
-const startScreen = document.querySelector('.start-screen')
-let player 
-playerX.addEventListener('click', () => { 
-  player = Player('X') 
-  startScreen.style.display = 'none'
-})
-playerO.addEventListener('click', () => { 
-  player = Player('O') 
-  startScreen.style.display = 'none'
-})
+  
+  Array.from(field.children).forEach((block, i) => {
+    block.addEventListener('click', () => {
+      block.innerHTML = Player.getSign()
+      gameBoard.updateBlock(i, Palyer.getSign())
+    })
+  })
+
+  const startGame = () => {
+    startScreen.style.display = 'none'
+    field.style.display = 'inline-grid'
+  }
+  
+  const endGame = () => {
+    const endScreen = document.querySelector('.end-game-screen')
+    endScreen.style.display = 'block'
+  }
+
+  const resetGame = () => {
+    startScreen.style.display = 'block'
+    gameBoard.reset()
+  }
+  
+  //all possible winning scenarios
+  const winScenarios = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+
+  
+
+  const checkWinningX = () => {
+    winScenarios.some((arr) => {
+      return arr.every((i) => gameBoard.getBlock(i) === 'X')
+    })
+  }
+
+  const checkWinningO = () => {
+    winScenarios.some((arr) => {
+      return arr.every((i) => gameBoard.getBlock(i) === 'X')
+    })
+  }
+
+
+  startBtn.addEventListener('click', startGame())
+
+})();
+
+
 
 
